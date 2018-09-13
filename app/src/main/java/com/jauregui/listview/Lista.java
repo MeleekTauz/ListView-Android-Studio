0c4ccbf4721e48141c2912bae9a7391c8072b166
Vidal Jauregui <vidaljagu@hotmail.com>
2018-09-13 02:06:13 -0500

    List view V.final

---
+++ app/src/main/java/com/jauregui/listview/Lista.java
@@ -0,0 +1,43 @@
+package com.jauregui.listview;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lista extends AppCompatActivity {
+
+    ListView lv1;
+    List<String> lista = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista);
+
+        //
+        setTitle("Lista de alumnos");
+        //
+
+        lv1 = (ListView) findViewById(R.id.lvAlumnos);
+
+        Intent btn1 = getIntent();
+
+        String nombre = btn1.getExtras().getString("nombre");
+        String apellido = btn1.getExtras().getString("apellido");
+        String edad = btn1.getExtras().getString("edad");
+        String mensaje = btn1.getExtras().getString("mensaje");
+        String correo = btn1.getExtras().getString("correo");
+
+        final ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,lista);
+
+        lv1.setAdapter(adaptador);
+
+        lista.add(nombre + " " + apellido + " " + edad + " años" + '\n' + mensaje + '\n' + "Correo: " + correo);
+
+    }
+}
--- app/src/main/java/com/jauregui/listview/MainActivity.java
+++ app/src/main/java/com/jauregui/listview/MainActivity.java
@@ -1,5 +1,6 @@
 package com.jauregui.listview;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,12 +15,13 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class MainActivity extends AppCompatActivity {
 
     EditText et1, et2, et3,et4;
     RadioButton rbt1;
-    ListView lv1;
     Button btn1;
+    //ListView lv1;
     List<String> lista = new ArrayList<>();
 
     @Override
@@ -27,13 +29,60 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //
+        setTitle("Ingrese Datos");
+        //
         et1 = (EditText) findViewById(R.id.etNombre);
         et2 = (EditText) findViewById(R.id.etApellido);
         et3 = (EditText) findViewById(R.id.etEdad);
         et4 = (EditText) findViewById(R.id.etCorreo);
+        //lv1 = (ListView) findViewById(R.id.List1);
         rbt1 = (RadioButton) findViewById(R.id.rbtAlumno);
-        btn1 = (Button) findViewById(R.id.rbtAlumno);
-        lv1 = (ListView) findViewById(R.id.lvAlumnos);
+        btn1 = (Button) findViewById(R.id.btAgregar);
+
+        //lista.add("Alumno :");
+        //final ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,lista);
+
+        //lv1.setAdapter(adaptador);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //lv1.setAdapter(null);
+
+                String mensaje = " no es alumno";
+                if (rbt1.isChecked()){
+                    mensaje = "es alumno";
+                }
+
+                String nombre = et1.getText().toString().trim(), apellido = et2.getText()
+                        .toString().trim(), edad = et3.getText().toString().trim(),
+                        correo = et4.getText().toString().trim();
+                lista.add(nombre);
+                lista.add(apellido);
+                lista.add(edad);
+                lista.add(mensaje);
+                lista.add(correo);
+
+                et1.setText(" ");
+                et2.setText(" ");
+                et3.setText(" ");
+                rbt1.isChecked();
+                et4.setText(" ");
+
+                //lv1.setAdapter(adaptador);
+
+               Intent btn1 = new Intent(MainActivity.this, com.jauregui.listview.Lista.class);
+               btn1.putExtra("nombre", nombre);
+               btn1.putExtra("apellido", apellido);
+               btn1.putExtra("edad", edad);
+               btn1.putExtra("mensaje", mensaje);
+               btn1.putExtra("correo", correo);
+               startActivity(btn1);
+
+            }
+        });
 
     }
 
